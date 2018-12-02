2ee83d116ffb129c132fd0ec6a965acf29add98d
Orpsemius <jvbteixeira@hotmail.com>
2018-12-02 01:35:41 -0300

    versao com placeholder de cadastro

--- ECO222/ECO222/src/app/app.module.ts
+++ ECO222/ECO222/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -11,6 +17,9 @@ import { MercadorPage } from '../pages/mercador/mercador';
 import { MercadorHomePage } from '../pages/mercador-home/mercador-home';
 import { CadastroDeMercadoriaPage } from '../pages/cadastro-de-mercadoria/cadastro-de-mercadoria';
 import { ClienteHomePage } from '../pages/cliente-home/cliente-home';
+import { ContatosProvider } from '../providers/contatos/contatos';
+import { ContactProvider } from '../providers/contact/contact';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +34,18 @@ import { ClienteHomePage } from '../pages/cliente-home/cliente-home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    HttpClientModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyDBdPNAfFpvwmkhWAQcvTGs4xwyGlff7CI",
+    authDomain: "eco2-3d45d.firebaseapp.com",
+    databaseURL: "https://eco2-3d45d.firebaseio.com",
+    projectId: "eco2-3d45d",
+    storageBucket: "eco2-3d45d.appspot.com",
+    messagingSenderId: "36591106240"
+    }),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -35,12 +55,15 @@ import { ClienteHomePage } from '../pages/cliente-home/cliente-home';
     MercadorPage,
     MercadorHomePage,
     CadastroDeMercadoriaPage,
-    ClienteHomePage 
+    ClienteHomePage,
+     
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ContatosProvider,
+    ContactProvider
   ]
 })
 export class AppModule {}
--- ECO222/ECO222/src/pages/home/home.ts
+++ ECO222/ECO222/src/pages/home/home.ts
@@ -1,16 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { ClientePage } from '../cliente/cliente';
 import { MercadorPage } from '../mercador/mercador';
+import { ContactProvider } from './../../providers/contact/contact';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  contacts: Observable<any>;
+  
+    constructor(
+    public navCtrl: NavController,
+    private provider: ContactProvider,
+    private toast: ToastController) {
 
-  constructor(public navCtrl: NavController) {
+      this.contacts = this.provider.getAll();
+      
 
+
+  }
+
+  newContact(){
+    this.navCtrl.push('ContactPage');
+  }
+
+  editContact(contact:any){
+    this.navCtrl.push('ContactPage', {contact: contact});
+
+  }
+
+  removeContact(key:string){
+    this.provider.remove(key)
+    .then(() => {
+      this.toast.create({ message: 'Contato removido com sucesso.', duration:3000}).present();
+    })
+    .catch(() =>{
+      this.toast.create({ message: 'Erro ao remover o contato.', duration:3000}).present;
+    })
   }
 
   goToClientePage(){
